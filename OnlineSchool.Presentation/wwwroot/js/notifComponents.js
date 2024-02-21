e9e51b6e1e3daf4dfe7e8799fbfc59952165eeeb
Alexandr Arciuk <alexandr.arciuk@ebs-integrator.com>
2024-02-21 15:36:52 +0200

    real-users-names

---
+++ OnlineSchool.Presentation/wwwroot/js/notifComponents.js
@@ -0,0 +1,26 @@
+﻿
+function SawNotification(id) {
+
+    document.getElementById("Notification_" + id).remove();
+
+    notifCountInner = document.getElementById("notifCountInner");
+    notifCountInner.innerText = notifCountInner.innerText - 1;
+
+    let notifCount = document.getElementById("notifCount");
+    notifCount.innerText = notifCount.innerText - 1;
+
+    if (notifCount.innerText == 0)
+        notifCount.innerText = "";
+
+    $.ajax({
+        type: "POST",
+        url: "/Home/SawNotification",
+        data: {
+            Id: id,
+        },
+        success: function (result) {
+            // Handle the result here
+            console.log(result);
+        }
+    });
+}
